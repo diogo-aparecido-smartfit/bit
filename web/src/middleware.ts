fdacbff8b1738ab8adcadb06edaa2c1692272f7c
DiogoAMV <diogo.amv19@gmail.com>
2024-02-25 17:51:19 -0300

    fix: add auth verification to /dashboard

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -2,12 +2,12 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { isAuthenticated } from "./app/utils/isAuthenticated";
 
-const protectedRoutes = ["/dashboard", "/dashboard/edit", "/dashboard/posts"];
+const protectedRoutes = "/dashboard";
 
 export default async function middleware(req: NextRequest) {
   const result = await isAuthenticated();
 
-  if (result !== true && protectedRoutes.includes(req.nextUrl.pathname)) {
+  if (result !== true && req.nextUrl.pathname.includes(protectedRoutes)) {
     const absoluteURL = new URL("/signin", req.nextUrl.origin);
     return NextResponse.redirect(absoluteURL.toString());
   }
